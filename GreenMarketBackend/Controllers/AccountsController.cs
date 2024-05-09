100917bed42d204c1b36ab78431424b7bd2006a0
Gorgi Kostov <39196442+furiouskopan@users.noreply.github.com>
2024-05-09 16:35:40 +0200

    Add project files.

---
+++ GreenMarketBackend/Controllers/AccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenMarketBackend.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ GreenMarketBackend/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenMarketBackend.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}

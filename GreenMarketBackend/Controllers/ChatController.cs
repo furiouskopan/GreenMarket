cf4ba3be351118a8eb97ed645cc831d5eed51bad
Gorgi Kostov <39196442+furiouskopan@users.noreply.github.com>
2024-06-18 13:46:03 +0200

    Added link to chat at account page

--- GreenMarketBackend/Controllers/ChatController.cs
+++ GreenMarketBackend/Controllers/ChatController.cs
@@ -23,25 +23,25 @@ namespace GreenMarketBackend.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? sessionId)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var chatSessions = await _context.ChatSessions
                 .Where(cs => cs.User1Id == userId || cs.User2Id == userId)
                 .Include(cs => cs.Messages)
                 .ThenInclude(m => m.Sender)
-                .Include(cs => cs.User1) // Include User1
-                .Include(cs => cs.User2) // Include User2
+                .Include(cs => cs.User1)
+                .Include(cs => cs.User2)
                 .ToListAsync();
 
             var viewModel = new ChatViewModel
             {
-                ChatSessions = chatSessions
+                ChatSessions = chatSessions,
+                SelectedChatSessionId = sessionId ?? 0
             };
 
             return View(viewModel);
         }
-
 
         [HttpPost]
         public async Task<IActionResult> StartChatByUsername([FromBody] StartChatRequest model)
@@ -77,8 +77,6 @@ namespace GreenMarketBackend.Controllers
         {
             public string Username { get; set; }
         }
-
-
 
         [HttpPost]
         public async Task<IActionResult> SendMessage([FromBody] MessageModel model)
@@ -118,10 +116,59 @@ namespace GreenMarketBackend.Controllers
             await _context.SaveChangesAsync();
 
             var recipientId = (chatSession.User1Id == userId) ? chatSession.User2Id : chatSession.User1Id;
-            await _hubContext.Clients.User(recipientId.ToString()).SendAsync("ReceiveMessage", sender.UserName, model.Content);
+            await _hubContext.Clients.User(recipientId).SendAsync("ReceiveMessage", sender.UserName, model.Content);
 
             return Ok();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> StartChatWithUser(string userName)
+        {
+            var initiatorId = _userManager.GetUserId(User);
+            var targetUser = await _userManager.FindByNameAsync(userName);
+
+            if (targetUser == null)
+            {
+                return NotFound();
+            }
+
+            var existingSession = await _context.ChatSessions
+                .FirstOrDefaultAsync(cs =>
+                    (cs.User1Id == initiatorId && cs.User2Id == targetUser.Id) ||
+                    (cs.User1Id == targetUser.Id && cs.User2Id == initiatorId));
+
+            if (existingSession == null)
+            {
+                var newSession = new ChatSession
+                {
+                    User1Id = initiatorId,
+                    User2Id = targetUser.Id
+                };
+                _context.ChatSessions.Add(newSession);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", new { sessionId = newSession.Id });
+            }
+
+            return RedirectToAction("Index", new { sessionId = existingSession.Id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteChatSession(int chatSessionId)
+        {
+            var chatSession = await _context.ChatSessions
+                .Include(cs => cs.Messages)
+                .FirstOrDefaultAsync(cs => cs.Id == chatSessionId);
+
+            if (chatSession == null)
+            {
+                return NotFound();
+            }
+
+            _context.Messages.RemoveRange(chatSession.Messages);
+            _context.ChatSessions.Remove(chatSession);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }

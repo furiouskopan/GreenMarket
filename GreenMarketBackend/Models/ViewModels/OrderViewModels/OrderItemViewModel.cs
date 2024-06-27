6360afc6d9d1f777dc0951967043ea03856cd5ac
Gorgi Kostov <39196442+furiouskopan@users.noreply.github.com>
2024-06-27 14:01:00 +0200

    Order ViewModels

---
+++ GreenMarketBackend/Models/ViewModels/OrderViewModels/OrderItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GreenMarketBackend.Models.ViewModels.OrderViewModels
+{
+    public class OrderItemViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ GreenMarketBackend/Models/ViewModels/OrderViewModels/OrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using static GreenMarketBackend.Models.Order;
+
+namespace GreenMarketBackend.Models.ViewModels.OrderViewModels
+{
+    public class OrderViewModel
+    {
+        public int OrderId { get; set; }
+        public string UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string ShippingAddress { get; set; }
+        public string PaymentMethod { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItemViewModel> OrderItems { get; set; }
+    }
+}

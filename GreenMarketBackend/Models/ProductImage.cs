ea8cdd254dbdd69446013298a010ee400a3abe87
Gorgi Kostov <39196442+furiouskopan@users.noreply.github.com>
2024-09-10 16:59:50 +0200

    Product Image model

---
+++ GreenMarketBackend/Models/ProductImage.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GreenMarketBackend.Models
+{
+    public class ProductImage
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [Required]
+        public string ImageURL { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
